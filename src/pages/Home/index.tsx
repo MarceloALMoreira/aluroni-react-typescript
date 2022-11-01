f1a3d6328139fc171f8c6327c34524f55e269e32
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-11-01 01:29:37 -0300

    feat: criando dashboard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,15 +9,29 @@ export default function Home() {
                 <div className="conteudo">
                     <div className="row">
                         <div className="col-md-2">
-                            <h1>Patrimonio</h1>
+                            <div className="card">
+                                <img className='card__user' src="" alt="User" />
+                                <p className=''>Maria Gonçalve</p><hr />
+                            </div>
                         </div>
                         <div className="col-md-10">
-                            <h1>Conteudo</h1>
+                            <h1>Patrimônio</h1>   
+                            <div className="row">
+                                <div className="col-md-4 bg-dark ">
+                                    <h1>1</h1>
+                                </div>
+                                <div className="col-md-4 bg-dark ">
+                                    <h1>2</h1>
+                                </div>
+                                <div className="col-md-4 bg-dark ">
+                                    <h1>3</h1>
+                                </div>
+                            </div>                       
                         </div>
                     </div>
                 </div>
             </div>
-            
+
         </div>
     )
 }
