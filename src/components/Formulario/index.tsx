fb3ac36f8491bd86f7891a4df628bca87a9093c5
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-11-08 22:16:07 -0300

    feat: table

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,18 +1,9 @@
-import * as React from 'react';
-import { useForm, SubmitHandler } from "react-hook-form";
-import styles from './Formulario.module.scss';
 
-type Inputs = {
-    example: string,
-    exampleRequired: string,
-};
+import styles from './Formulario.module.scss';
 
 
 export default function Forms() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-    console.log(watch("example"))
 
 
 
@@ -24,17 +15,39 @@ export default function Forms() {
             <br />
             <br />
             <br />
-            <div className="col-md-10 border  container">
+            <div className="col-md-10   container">
                 <h3 className={styles.title}>teste</h3>
                 <div className={styles.title__form}>
-                    <div className="row">
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <input defaultValue="test" {...register("example")} />
-                            <input {...register("exampleRequired", { required: true })} />
-                            {errors.exampleRequired && <span>This field is required</span>}
-                            <input type="submit" />
-                        </form>
-                    </div>
+                    <table className="table table-hover text-white">
+                        <thead>
+                            <tr>
+                                <th scope="col">0</th>
+                                <th scope="col">1</th>
+                                <th scope="col">2</th>
+                                <th scope="col">3</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr className={ styles.table}>
+                                <th scope="row">3</th>
+                                <td>Larry the Bird</td>
+                                <td>@twitter</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
 
                 { /* https://blog.logrocket.com/using-material-ui-with-react-hook-form/ */}
