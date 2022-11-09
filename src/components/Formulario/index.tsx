55c802715bac7951efb2f29c52eb8e45f530796a
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-11-08 21:50:18 -0300

    feat: mundaça formulario

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,102 +1,46 @@
-import { Paper, TextField } from '@mui/material';
-import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import * as React from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
+import styles from './Formulario.module.scss';
+
+type Inputs = {
+    example: string,
+    exampleRequired: string,
+};
+
 
 export default function Forms() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+    console.log(watch("example"))
 
 
 
 
     return (
         <main>
-
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
             <div className="col-md-10 border  container">
-                <h3 className="text-white">Formulario</h3>
-
-
-                <div className="form container">
-                    <h2 className="">Consulta de Bens Importados</h2>
-                    <div className="">
-
-
-
-
-
-                        <Paper className="p-2">
-                            <h2>Form Demo</h2>
-                            <div className="m-2">
-                                <TextField
-                                    label={"Text Value"} //optional
-                                />
-                                <TextField className='col-md-3'
-                                        label={"Text Value"} //optional
-                                    />
-                            </div>
-                        </Paper>
-                        <br />
-                        <br />
-                        <br />
-                        <hr />
-
-                        { /*<form>
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-md-4">
-                                        <label className="control-label" htmlFor="">Filtrar por:</label>
-                                        <select className="form-select form-select-sm" aria-label="size 3 select example">
-                                            <option selected></option>
-                                        </select>
-                                    </div>
-                                    <div className="col-md-8"><br />
-                                        <input type="text" className="form-control form-control-sm" aria-label="" aria-describedby="basic-addon1" />
-                                    </div>
-                                </div> <br />
-                                <div className="row">
-                                    <div className="col-md-4">
-                                        <label className="control-label" htmlFor="">Secretaria:</label>
-                                        <select className="form-select form-select-sm" aria-label="size 3 select example">
-                                            <option selected></option>
-                                        </select>
-                                    </div>
-                                    <div className="col-md-4">
-                                        <label className="control-label" htmlFor="">Centro de custo:</label>
-                                        <select className="form-select form-select-sm" aria-label="size 3 select example">
-                                            <option selected></option>
-                                        </select>
-                                    </div>
-                                    <div className="col-md-4">
-                                        <label className="control-label" htmlFor="">Localização:</label>
-                                        <select className="form-select form-select-sm" aria-label="size 3 select example">
-                                            <option selected></option>
-                                        </select>
-                                    </div>
-                                    <div className="p-1"> <br />
-                                        <a className="btn btn-primary m-1">Filtrar</a>
-                                        <a className="btn btn-danger">Limpar</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </form >
-                        https://blog.logrocket.com/using-material-ui-with-react-hook-form/
-                        */}
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Filtrar Por:</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Form.Label htmlFor="disabledSelect form-select form-select-sm">Disabled select menu</Form.Label>
-                                <Form.Select id="disabledSelect">
-                                    <option>a</option>
-                                </Form.Select>
-                            </Form.Group>
-                        </Form>
+                <h3 className={styles.title}>teste</h3>
+                <div className={styles.title__form}>
+                    <div className="row">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input defaultValue="test" {...register("example")} />
+                            <input {...register("exampleRequired", { required: true })} />
+                            {errors.exampleRequired && <span>This field is required</span>}
+                            <input type="submit" />
+                        </form>
                     </div>
                 </div>
+
+                { /* https://blog.logrocket.com/using-material-ui-with-react-hook-form/ */}
+
+
+
             </div>
         </main>
 
