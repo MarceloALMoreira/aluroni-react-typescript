e6e709fa716341bd2ab04ef5ad7a6a3be045c924
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-11-10 02:23:13 -0300

    feat: estilizando sidbar

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react"
-
-
 import { MdSpaceDashboard } from 'react-icons/md';
 import { RiDashboard2Fill } from 'react-icons/ri';
 import { FaAddressCard, FaTaxi } from 'react-icons/fa';
@@ -11,55 +9,64 @@ import { FiLogOut } from 'react-icons/fi';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { VscChromeClose } from 'react-icons/vsc';
 import styles from './Dashboard.module.scss';
+import { style } from "@mui/system";
 
 
 
 
 export default function Sidebar() {
+    const [currentLink, setCurrentLink] = useState(1)
+
     return (
         <>
             <div className={styles.section}>
                 <div className={styles.section__top}>
                     <div className={styles.section__brand}>
                         <FaTaxi />
-                        <span>Cliente</span> 
+                        <span>cliente</span>
                         <hr />
                     </div>
                     <div className={styles.section__toggle}>
                     </div>
                     <div className={styles.section__links}>
                         <ul>
-                            <li>
+                            <li onClick={() => setCurrentLink(1)}
+                                className={currentLink === 1 ? "active" : ""}>
                                 <a href="">
                                     <MdSpaceDashboard />
                                     <span>Dashboard</span>
                                 </a>
                             </li>
-                            <li>
+                            <li onClick={() => setCurrentLink(2)}
+                                className={currentLink === 2 ? "active" : ""}>
                                 <a href="">
                                     <RiDashboard2Fill />
                                     <span>Riders</span>
                                 </a>
                             </li>
-                            <li>
+                            <li onClick={() => setCurrentLink(3)}
+                                className={currentLink === 3 ? "active" : ""}>
                                 <a href="">
                                     <FaAddressCard />
                                     <span>Payment Details</span>
                                 </a>
                             </li>
-                            <li>
+                            <li onClick={() => setCurrentLink(4)}
+                                className={currentLink === 4 ? "active" : ""}>
                                 <a href="">
                                     <GiTwirlCenter />
                                     <span>Learning Center</span>
                                 </a>
                             </li>
-                            <li>
+                            <li onClick={() => setCurrentLink(5)}
+                                className={currentLink === 5 ? "active" : ""}>
                                 <a href="">
                                     <BsFillChatTextFill />
                                     <span>FAQs</span>
                                 </a>
                             </li>
-                            <li>
+                            <li onClick={() => setCurrentLink(6)}
+                                className={currentLink === 6 ? "active" : ""}>
                                 <a href="">
                                     <IoSettings />
                                     <span>Settings</span>
@@ -70,7 +77,7 @@ export default function Sidebar() {
                 </div>
                 <div className="logout">
                     <a href="">
-                        <FiLogOut/>
+                        <FiLogOut />
                         <span className="logout">Logout</span>
                     </a>
                 </div>
