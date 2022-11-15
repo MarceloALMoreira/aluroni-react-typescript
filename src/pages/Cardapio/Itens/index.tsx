2d295257aa8f265fac6960ecc08701f0c36ba71f
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-11-15 10:09:47 -0300

    feat: projeto final

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -6,34 +6,46 @@ import { useEffect, useState } from 'react';
 interface Props {
     busca: string,
     filtro: number | null,
-    ordenador: string,
-}
-
-export default function Itens(props: Props) {
+    ordenador: string
+  }
+  
+  export default function Itens(props: Props) {
     const [lista, setLista] = useState(Cardapio);
-    const { busca, filtro } = props; 
-
-
+    const { busca, filtro, ordenador } = props;
+  
     function testaBusca(title: string) {
-        const regex = new RegExp(busca, 'i');
-        return regex.test(title);
+      const regex = new RegExp(busca, 'i');
+      return regex.test(title);
     }
+  
     function testaFiltro(id: number) {
-        if (filtro !== null) return filtro === id;
-        return true;
+      if(filtro !== null) return filtro === id;
+      return true;
     }
-
-
+  
+    function ordenar(novaLista: typeof Cardapio) {
+      switch(ordenador) {
+        case 'porcao': 
+          return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+        case 'qtd_pessoas':
+          return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
+        case 'preco':
+          return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
+        default:
+          return novaLista; 
+      }
+    }
+  
     useEffect(() => {
-        const novaLista = Cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
-        setLista(novaLista);
-    },[busca, filtro])
-
+      const novaLista = Cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+      setLista(ordenar(novaLista));
+    },[busca, filtro, ordenador])
+  
     return (
-        <div className={styles.itens}>
-            {lista.map(item => (
-                <Item key={item.id}{...item} />
-            ))}
-        </div>
+      <div className={styles.itens}>
+        {lista.map(item => (
+          <Item key={item.id} {...item} />
+        ))}
+      </div>
     )
-}
+  }
