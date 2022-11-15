874be7fdc4bb423b5541e8f2ce75bb3572efcbe1
MarceloAlMoreira <marceloaomoreira@hotmail.com>
2022-11-15 10:39:55 -0300

    fix: remove home

--- src/components/Dashboard/Earnings.tsx
+++
@@ -1,7 +0,0 @@
-export default function Earnings() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/components/Dashboard/Transfers.tsx
+++
@@ -1,5 +0,0 @@
-export default function Transfers() {
-    return (
-        <div></div>
-    )
-}
